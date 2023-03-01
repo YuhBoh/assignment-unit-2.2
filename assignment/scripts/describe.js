9a33f7be9438be41346303e69fa46a847ac5a519
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-02-28 23:17:08 -0800

    finished assignment in describe.js

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,9 +26,13 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
+// assign variable "name" the value of 'Dane'
 // 
+// set conditional statement: if variable "name"'s value is strictly equal to that of 'Mary''s, 
+//                            then log 'Hi, Mary!' into console.
 // 
-// 
+// else: if variable "name"'s value is NOT strictly equal to that of 'Mary''s, 
+//       then Javascript will log the string 'How do you do?' into console.
 //
 
 //CODE
@@ -45,10 +49,18 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
+// assign variable 'code' with value '123' and a variable called 'secret'.
 // 
+// set conditional statement: if variable 'code''s numerical value is exactly 123, 
+//                            then variable 'secret' is now 'super AND
+//                            'code''s value(123) is now multiplied by 2(123*2=246)
 // 
+// second conditional statement: if value of 'code'(246) is higher than 250, 
+//                               then variable 'secret''s value is now 'duper'.
 // 
-//
+// log the value of 'secret' into console. 
+// Because the numerical value of 'code'(246) is NOT higher than 250, 
+// the variable 'secret' will have a value of the string 'super' when logged into console.
 
 //CODE
 /*
@@ -70,10 +82,25 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
+// Variable 'isStudent' is assigned boolean value of 'true' 
+// Variable 'age' is assigned number data of 34 
+// Variable 'zip' is assigned number data of 55407
 // 
+// set conditional statement: if the variable 'isStudent''s boolean value is strictly equal to 'true' AND 
+//                            if the variable 'zip''s number value is higher than 80000, 
+//                            then Javascript will log, `You're a student on the West Coast!` into the console.
 // 
+//else if: given the previous conditional statement's boolean value is 'false',
+//         if variable 'isStudent''s boolean value is strictly 'false' OR 
+//         the numberical value of variable 'age' is less than 30,
+//         the string, 'What are your hobbies?' will be logged into console.
 // 
-//
+// else if: given the previous 'else if' conditional statement's boolean value is 'false',
+//          if variable 'isStudent''s boolean value is strictly 'true',
+//          the string, 'Welcome to Prime!' will be logged into console.
+// 
+// else: given the previous conditional statements' boolean value are all 'false',
+//       the string, 'How about the weather?' will be logged into console.
 
 //CODE
 /*
@@ -130,6 +157,16 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
+// PROBLEM #1 - variable 'colorOne' is assigned 'red' AND
+//              variable 'colorTwo' is assigned 'blue
+// 
+// FIX #1 - assign variable 'colorOne' to 'blue' AND
+//          assign variable 'colorTwo' to 'red'
+// 
+// PROBLEM #2 - variable 'colorTwo' is not assigned 'purple'
+// 
+// FIX #2 - assign variable 'colorTWO' to 'purple'
+
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
@@ -144,6 +181,11 @@ if (mix === true) {
 // -- they are so we console.log 'throw away the food!'
 
 /*
+// PROBLEM - the operator '||' has a boolean value of 'or'
+// 
+// FIX - change operator '||' to '&&' 
+//       ('&&' has a boolean value of 'and')
+
 let temp = 40;
 const time = 4;
 
@@ -157,6 +199,17 @@ if (temp > 39 || time >= 4) {
 // -- it is, so we console.log 'enter'.
 
 /*
+// PROBLEM #1 - the operator '<=' has a value of 'less than or equal to'
+// 
+// FIX #1 - change operator '<=' to '>='
+//          ('>=' has a value of 'greater than or equal to')
+// 
+// PROBLEM #2 - the conditional statement 'else' means that 'given previous conditional statements are false/not true'
+// 
+// FIX #2 - delete 'else' statement and put 'console.log('enter');' into the 'if' statements code block AND
+//          one line below 'console.log('no entry');'
+//          (all code blocks in conditional statements are between curly brackets({}) )
+
 let age = 21;
 const minAge = 21;
 
